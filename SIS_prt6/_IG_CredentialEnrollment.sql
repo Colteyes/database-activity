5e3355c2cf51ab51aa5a187d20186bc49f87feff
Colt_eyes13 <colteyes13@gmail.com>
2026-04-10 23:12:53 -0600

    Changes to my files

---
+++ SIS_prt6/_IG_CredentialEnrollment.sql
@@ -0,0 +1,15 @@
+-- REPORT 3: Credential Enrollment Summary
+
+SELECT 
+    c.name AS credential_name,
+    COUNT(sc.student_id) AS total_students
+FROM sis_credential c
+LEFT OUTER JOIN sis_student_credential sc   -- outer join
+    ON c.credential_id = sc.credential_id
+WHERE c.credential_id IN (
+        SELECT credential_id   -- subquery
+        FROM sis_student_credential
+        WHERE gpa > 3.2        -- condition inside subquery
+    )
+GROUP BY c.name
+ORDER BY total_students DESC;
